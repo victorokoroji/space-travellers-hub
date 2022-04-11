4c00a76995e5006ca65a2b03e3ce13f8a98b8892
unknown <okorojiebube2@gmail.com>
2022-04-11 17:53:13 -0600

    Add mission reducer

--- src/redux/missions/mission.js
+++ src/redux/missions/mission.js
@@ -14,15 +14,19 @@ export const getMissions = () => async dispatch => {
 }
 
 const missionReducer = (state = initialState, action) => {
-  const {payload} = action
-  switch (action.type) {
-    case GET_MISSIONS:
-      return {
-        ...Object.entries(payload).map((mission) => {
-            id: mission[0]
-          } )
-      }
-    default:
-      break;
-  }
+	const { payload } = action
+	switch (action.type) {
+		case GET_MISSIONS:
+			return {
+				...Object.entries(payload).map(mission => ({
+					mission_id: mission[0],
+					mission_name: mission[1],
+					description: mission[2],
+				})),
+			}
+		default:
+			break
+	}
 }
+
+export missionReducer
