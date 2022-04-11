cde297983b4339be65692a98008b295db880ee0e
unknown <okorojiebube2@gmail.com>
2022-04-11 16:24:11 -0600

    Add mission component

---
+++ src/components/Mission.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Mission = () => {
+  return (
+    <table>
+      <th>
+        <td>Mission</td>
+        <td>Description</td>
+        <td>Status</td>
+        <td></td>
+      </th>
+      <tr>
+        <td>Vic</td>
+        <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum quo harum possimus pariatur ab fuga non sint doloremque! Nihil nesciunt, quam dolore accusantium sed enim quis quisquam. Vel, dolor! Voluptatibus!</td>
+        <td>not a member</td>
+        <td><button>Join mission</button></td>
+      </tr>
+      <tr>
+        <td>Vic</td>
+        <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum quo harum possimus pariatur ab fuga non sint doloremque! Nihil nesciunt, quam dolore accusantium sed enim quis quisquam. Vel, dolor! Voluptatibus!</td>
+        <td>not a member</td>
+        <td><button>Join mission</button></td>
+      </tr>
+      <tr>
+        <td>Vic</td>
+        <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum quo harum possimus pariatur ab fuga non sint doloremque! Nihil nesciunt, quam dolore accusantium sed enim quis quisquam. Vel, dolor! Voluptatibus!</td>
+        <td>not a member</td>
+        <td><button>Join mission</button></td>
+      </tr>
+    </table>
+  )
+}
+
+export default Mission
---
+++ src/pages/MissionsPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Mission from '../components/Mission'
+
+const Missions = () => (
+	<div>
+		<Mission />
+	</div>
+)
+export default Missions
