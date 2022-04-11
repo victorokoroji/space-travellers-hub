6bfb6f17d9da0114908ed81cddec72461691869b
unknown <okorojiebube2@gmail.com>
2022-04-11 14:57:24 -0600

    Add Mission component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import { Route, Routes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
+import Missions from './pages/Missions'
+import Rockets from './pages/Rockets'
+import Profile from './pages/Profile'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Routes>
-        <Route path='/' exact element={<Rockets />}/>
-        <Route path='/missions' exact element={<Missions />}/>
+        <Route path='/' exact element={<Rockets />} />
+        <Route path='/missions' element={<Missions />} />
+        <Route path='/profile' element={<Profile />} />
       </Routes>
     </div>
   );
