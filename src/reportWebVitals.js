32e0e7070fd7e4f59b6586c34a08569105829015
Abenezer-Tilahun <abenezermesafint2128@gmail.com>
2022-04-11 21:06:20 +0300

    Setup dependencies for project

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,6 +1,8 @@
-const reportWebVitals = onPerfEntry => {
+const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+    import('web-vitals').then(({
+      getCLS, getFID, getFCP, getLCP, getTTFB,
+    }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
@@ -9,5 +11,4 @@ const reportWebVitals = onPerfEntry => {
     });
   }
 };
-
 export default reportWebVitals;
