b1441243bdeb8de7998a1bbcb382b99924667844
unknown <okorojiebube2@gmail.com>
2022-04-12 14:56:01 -0600

    Populate profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
 const Profile = () => (
-  <section className='profile'>
-     
-  </section>
-);
+	<section className='profile'>
+		<div className='profile-list'>
+			<div className='join-mission'>
+				<h2>My Missions</h2>
+				<ul>
+					<li>jjjjjjjjj</li>
+					<li>jjjjjjjjj</li>
+					<li>jjjjjjjjj</li>
+					<li>jjjjjjjjj</li>
+					<li>jjjjjjjjj</li>
+				</ul>
+			</div>
+			<div className='reserved-rockets'>
+				<h2>My Rockets</h2>
+				<ul>
+					<li>jjjjjjjjj</li>
+					<li>jjjjjjjjj</li>
+					<li>jjjjjjjjj</li>
+					<li>jjjjjjjjj</li>
+					<li>jjjjjjjjj</li>
+				</ul>
+			</div>
+		</div>
+	</section>
+)
 
 export default Profile;
