49bbe60b07daedf19e4d6183d7085fa2de2252f7
unknown <okorojiebube2@gmail.com>
2022-04-12 17:04:39 -0600

    Fix linter errors

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,49 +1,48 @@
 import React from 'react';
-import { useSelector, shallowEqual } from 'react-redux';
+import { useSelector, shallowEqual, useDispatch } from 'react-redux';
 import Button from '../components/Button';
-import { useDispatch } from 'react-redux'
-import { leaveMission } from '../redux/missions/mission'
+import { leaveMission } from '../redux/missions/mission';
 
 const Profile = () => {
   const missions = useSelector((state) => state.missionReducer, shallowEqual);
-	const dispatch = useDispatch()
-	
+  const dispatch = useDispatch();
+
   return (
-		<section className='profile'>
-			<div className='profile-list'>
-				<div className='join-mission'>
-					<h2>My Missions</h2>
-					{missions[0] ? (
-						<ul className='list'>
-							{missions.map(mission => (
-								<li key={mission.mission_id}>
-									<span>{mission.mission_name}</span>
-									<Button
-										className='join'
-										onClick={() => dispatch(leaveMission(mission.mission_id))}
-									>
-										Leave Mission
-									</Button>
-								</li>
-							))}
-						</ul>
-					) : (
-						<div className='message'> No missions joined yet</div>
-					)}
-				</div>
-				<div className='reserved-rockets'>
-					<h2>My Rockets</h2>
-					<ul className='list'>
-						<li>jjjjjjjjj</li>
-						<li>jjjjjjjjj</li>
-						<li>jjjjjjjjj</li>
-						<li>jjjjjjjjj</li>
-						<li>jjjjjjjjj</li>
-					</ul>
-				</div>
-			</div>
-		</section>
-	)
+    <section className="profile">
+      <div className="profile-list">
+        <div className="join-mission">
+          <h2>My Missions</h2>
+          {missions[0] ? (
+            <ul className="list">
+              {missions.map((mission) => (
+                <li key={mission.mission_id}>
+                  <span>{mission.mission_name}</span>
+                  <Button
+                    className="join"
+                    onClick={() => dispatch(leaveMission(mission.mission_id))}
+                  >
+                    Leave Mission
+                  </Button>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <div className="message"> No missions joined yet</div>
+          )}
+        </div>
+        <div className="reserved-rockets">
+          <h2>My Rockets</h2>
+          <ul className="list">
+            <li>jjjjjjjjj</li>
+            <li>jjjjjjjjj</li>
+            <li>jjjjjjjjj</li>
+            <li>jjjjjjjjj</li>
+            <li>jjjjjjjjj</li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Profile;
