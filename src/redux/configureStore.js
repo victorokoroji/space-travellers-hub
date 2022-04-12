0c14a35b43a446a903f2f958a156e4b0e7f34217
Abenezer-Tilahun <abenezermesafint2128@gmail.com>
2022-04-12 18:41:58 +0300

    Add a Rocket Reducer

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,19 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import missionReducer from './missions/mission';
+import rocketReducer from './rockets/rockets';
 
-const rootReducer = combineReducers({ missionReducer });
-const store = createStore(rootReducer, applyMiddleware(logger, thunk));
+const rootReducer = combineReducers({
+  missions: missionReducer,
+  rockets: rocketReducer,
+});
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(
+    logger,
+    thunk,
+  ),
+);
 export default store;
---
+++ src/redux/rockets/rockets.js
@@ -0,0 +1,9 @@
+const initialState = [];
+const rocketReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default rocketReducer;
