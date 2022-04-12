7d5dc30bf9821378146159d4cd11de99d6ee642a
unknown <okorojiebube2@gmail.com>
2022-04-12 03:05:18 -0600

    Get store from redux

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals'
+import { Provider } from 'react-redux'
+import store from './redux/configureStore'
 
+console.log(store)
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+      <Provider store={store}>
       <App />
+    </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
--- src/pages/MissionsPage.js
+++ src/pages/MissionsPage.js
@@ -1,5 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Mission from '../components/Mission'
+import { useSelector, useDispatch } from 'react-redux'
+import { getMissions } from '../redux/missions/mission'
+
+const missions = useSelector(state => state.missionReducer)
+console.log(missions)
+const dispatch = useDispatch()
+
+useEffect(() => {
+	dispatch(getMissions())
+}, [])
 
 const Missions = () => (
 	<div className='mission'>
