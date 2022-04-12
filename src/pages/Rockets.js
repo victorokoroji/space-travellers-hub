1ee45ed1b7956201c75cb25ee5713187caaed1dc
Abenezer-Tilahun <abenezermesafint2128@gmail.com>
2022-04-12 19:10:51 +0300

    Fetch data from API and Create action for rockets

--- src/pages/Rockets.js
+++ src/pages/Rockets.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchRocketsFromServer } from '../redux/rockets/rockets';
 
-const Rockets = () => (
-  <div>Rockets</div>
-);
+const Rockets = () => {
+  const rockets = useSelector((state) => state.rockets);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!rockets.length) dispatch(fetchRocketsFromServer());
+  }, []);
+
+  return (
+    <div className="rocket">
+      {rockets.map((rocket) => {
+        const {
+          id, name, type, images,
+        } = rocket;
+
+        return (
+          <div key={id} className="eachRocket">
+            <img src={images} alt="rocket" className="rocket-image" />
+            <p>{name}</p>
+            <p>{type}</p>
+          </div>
+        );
+      })}
+      ;
+    </div>
+  );
+};
 
 export default Rockets;
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,19 +1,13 @@
-import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import missionReducer from './missions/mission';
 import rocketReducer from './rockets/rockets';
 
 const rootReducer = combineReducers({
-  missions: missionReducer,
+  missionReducer,
   rockets: rocketReducer,
 });
 
-const store = createStore(
-  rootReducer,
-  applyMiddleware(
-    logger,
-    thunk,
-  ),
-);
+const store = createStore(rootReducer, applyMiddleware(logger, thunk));
 export default store;
