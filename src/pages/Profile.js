813bfaa147426aef53df9bccca74ef26e67feb15
Abenezer-Tilahun <abenezermesafint2128@gmail.com>
2022-04-13 09:54:40 +0300

    Add reserved booking to profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -5,6 +5,7 @@ import { leaveMission } from '../redux/missions/mission';
 
 const Profile = () => {
   const missions = useSelector((state) => state.missionReducer, shallowEqual);
+  const rockets = useSelector((state) => (state.rockets.filter((rocket) => rocket.reserved)));
   const dispatch = useDispatch();
 
   return (
@@ -33,11 +34,13 @@ const Profile = () => {
         <div className="reserved-rockets">
           <h2>My Rockets</h2>
           <ul className="list">
-            <li>jjjjjjjjj</li>
-            <li>jjjjjjjjj</li>
-            <li>jjjjjjjjj</li>
-            <li>jjjjjjjjj</li>
-            <li>jjjjjjjjj</li>
+            {
+            rockets.map((rocket) => (
+              <li className="item" key={rocket.id}>
+                {rocket.name}
+              </li>
+            ))
+          }
           </ul>
         </div>
       </div>
