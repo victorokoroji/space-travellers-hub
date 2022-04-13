cf0a5935f7702df4a99ceb7bfad62a8c295dc9e6
unknown <okorojiebube2@gmail.com>
2022-04-13 15:16:23 -0600

    Modify Mission component test

--- src/_tests_/rockets.test.js
+++ src/_tests_/rockets.test.js
@@ -1,11 +1,9 @@
 import missionReducer from '../redux/missions/mission'
-import {getMissions} from '../redux/missions/mission'
+import { getMissions } from '../redux/missions/mission'
 
 describe('reducer should return lists of missions', () => {
-  it('should return lists of objects in the state', () => {
+	it('should return lists of objects in the state', () => {
 		const previousState = []
 		expect(missionReducer(previousState, getMissions())).toBeDefined()
 	})
 })
-
-
